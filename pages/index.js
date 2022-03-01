e03007a567e5bb589108ddb067b4be291aee195d
Marco <marco@t360degrees.com>
2022-03-01 20:39:18 +0000

    Added main homepage pic. Edited homepage to have a basic website. TODO: build the structure of the website; develop different pages.

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,47 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import mainPicture from '../public/assets/clutch-e-bag-fashion-tech-made-in-italy-home-pic.jpg'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Clutch-e Bag</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Clutch-e Bag <br/>Coming Soon</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          FashionTech Innovation Made in Italy
         </p>
 
+        <Image
+          src={mainPicture}
+          alt="Homepage clutch-e bag main picture fashion tech made in italy startup"
+        />
+
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.forbes.fr/mediasfrance/le-clutch-e-bag-le-sac-intelligent-moderne-et-elegant/" className="card">
+            <h3>Forbes &rarr;</h3>
+            <p>Read Forbes' article about Clutch-e Bag</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://www.marieclaire.fr/adresses-incontournables/clutch-e-bag-la-pochette-connectee-made-in-italy/" className="card">
+            <h3>Marie Claire &rarr;</h3>
+            <p>Read Marie Claire's article about Clutch-e Bag</p>
           </a>
         </div>
       </main>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/company/15798268"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by Clutch-e Bag srl
         </a>
       </footer>
 
