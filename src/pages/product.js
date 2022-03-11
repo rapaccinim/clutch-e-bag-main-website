77162a00a858470606ab34c4feda716485c9023d
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2022-03-11 16:37:18 +0100

    Added Accordion to page

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,7 +1,16 @@
+import Accordion from "../components/Accordion/accordion"
+
 const Product = () => {
     return (
         <div className="container">
             <p>Product Page</p>
+            <Accordion items={
+                [
+                    {title: "Titolo 1", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quo et! Obcaecati magnam excepturi consectetur iste. Doloremque consequatur beatae enim voluptates officia, cum exercitationem sapiente autem dignissimos iste voluptatum fuga."},
+                    {title: "Titolo 2", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quo et! Obcaecati magnam excepturi consectetur iste. Doloremque consequatur beatae enim voluptates officia, cum exercitationem sapiente autem dignissimos iste voluptatum fuga."},
+                ]
+            } />
+            
             <style jsx>{`
                 // CSS-START
                 // insert here the CSS for this page
