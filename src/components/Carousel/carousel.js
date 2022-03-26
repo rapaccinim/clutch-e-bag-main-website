c9023d26f4bb82b54085c105d33bbd7bf1e2749e
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2022-03-26 18:20:45 +0100

    Implemented Carousel

---
+++ src/components/Carousel/carousel.js
@@ -0,0 +1,176 @@
+import { useState } from "react";
+
+const Carousel = (props) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const next = () => {
+    const images = props.items.length;
+    setCurrentSlide((currentSlide + 1) % images);
+  };
+
+  const prev = () => {
+    const images = props.items.length;
+    let current = currentSlide;
+
+    current -= 1;
+    if (current < 0) {
+      current = images - 1;
+    }
+
+    setCurrentSlide(current);
+  };
+
+  return (
+    <>
+      <div className="slideshow">
+        <div className="slideshow-container">
+          {props.items.map((el, index) => (
+            <div
+              className="mySlides fade"
+              hidden={currentSlide == index ? false : true}
+            >
+              <div className="numbertext">
+                {index + 1} / {props.items.length}
+              </div>
+              <img src={el.image} style={{ width: "100%" }} />
+            </div>
+          ))}
+
+          <a className="prev" onClick={prev}>
+            &#10094;
+          </a>
+          <a className="next" onClick={next}>
+            &#10095;
+          </a>
+        </div>
+
+        <div className="dot-container">
+          {props.items.map((_, index) => (
+            <span
+              className="dot"
+              onClick={() => {
+                setCurrentSlide(index);
+              }}
+            ></span>
+          ))}
+        </div>
+      </div>
+      <style jsx>{`
+        .slideshow {
+          position: relative;
+          width: 100%;
+        }
+        
+        .slideshow-container {
+          max-width: 1000px;
+          max-height: 100vh;
+          position: relative;
+          margin: auto;
+          overflow: hidden;
+        }
+        
+        .dot-container {
+          position: absolute;
+          bottom: 12px;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+        
+        /* Next & previous buttons */
+        .prev,
+        .next {
+          cursor: pointer;
+          position: absolute;
+          top: 50%;
+          width: auto;
+          margin-top: -22px;
+          padding: 16px;
+          color: white;
+          font-weight: bold;
+          font-size: 18px;
+          transition: 0.6s ease;
+          border-radius: 0 3px 3px 0;
+          user-select: none;
+        }
+        
+        /* Position the "next button" to the right */
+        .next {
+          right: 0;
+          border-radius: 3px 0 0 3px;
+        }
+        
+        /* On hover, add a black background color with a little bit see-through */
+        .prev:hover,
+        .next:hover {
+          background-color: rgba(0, 0, 0, 0.8);
+        }
+        
+        /* Caption text */
+        .text {
+          color: #f2f2f2;
+          font-size: 15px;
+          padding: 8px 12px;
+          position: absolute;
+          bottom: 8px;
+          width: 100%;
+          text-align: center;
+        }
+        
+        /* Number text (1/3 etc) */
+        .numbertext {
+          color: #f2f2f2;
+          font-size: 12px;
+          padding: 8px 12px;
+          position: absolute;
+          top: 0;
+        }
+        
+        /* The dots/bullets/indicators */
+        .dot {
+          cursor: pointer;
+          height: 15px;
+          width: 15px;
+          margin: 0 2px;
+          background-color: #bbb;
+          border-radius: 50%;
+          display: inline-block;
+          transition: background-color 0.6s ease;
+        }
+        
+        .active,
+        .dot:hover {
+          background-color: #717171;
+        }
+        
+        /* Fading animation */
+        .fade {
+          -webkit-animation-name: fade;
+          -webkit-animation-duration: 1.5s;
+          animation-name: fade;
+          animation-duration: 1.5s;
+        }
+        
+        @-webkit-keyframes fade {
+          from {
+            opacity: 0.4;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+        
+        @keyframes fade {
+          from {
+            opacity: 0.4;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+      `}
+      </style>
+    </>
+  );
+}
+
+export default Carousel;
--- src/pages/product.js
+++ src/pages/product.js
@@ -1,4 +1,5 @@
-import Accordion from "../components/Accordion/accordion"
+import Accordion from "../components/Accordion/accordion";
+import Carousel from "../components/Carousel/carousel";
 
 const Product = () => {
     return (
@@ -10,6 +11,23 @@ const Product = () => {
                     {title: "Titolo 2", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quo et! Obcaecati magnam excepturi consectetur iste. Doloremque consequatur beatae enim voluptates officia, cum exercitationem sapiente autem dignissimos iste voluptatum fuga."},
                 ]
             } />
+
+            <Carousel
+                items={[
+                  {
+                    image:
+                      "https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+                  },
+                  {
+                    image:
+                      "https://images.unsplash.com/photo-1605733513597-a8f8341084e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1229&q=80"
+                  },
+                  {
+                    image:
+                      "https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+                  }
+                ]}
+              />
             
             <style jsx>{`
                 // CSS-START
