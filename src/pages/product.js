0c1520e27d8c0c9afe02b95f9c73e797712097f4
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2022-03-27 12:21:27 +0200

    Added first section

--- src/pages/product.js
+++ src/pages/product.js
@@ -3,7 +3,40 @@ import Carousel from "../components/Carousel/carousel";
 
 const Product = () => {
     return (
-        <div className="container">
+        <div className="container-fluid">
+            <section className="flex hero-section">
+                <div className="flex-basis-50">
+                    <Carousel
+                        sizeFull={true}
+                        items={[
+                        {
+                            image:
+                            "https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+                        },
+                        {
+                            image:
+                            "https://images.unsplash.com/photo-1605733513597-a8f8341084e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1229&q=80"
+                        },
+                        {
+                            image:
+                            "https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+                        }
+                        ]}
+                    />
+                </div>
+                <div className="hero-section__description flex flex-col flex-basis-50">
+                    <h1 className="">Clutch-e bag</h1>
+                    <p>
+                    The first Smart Fashion Tech pochette in the world.
+                    The objective is that of bringing a new idea of handbag.
+
+                    Clutch-e Bag is equipped with fingerprint scanner for the opening, GPS tracking system, speaker to make it ring and find it easily everytime everywhere, removable power bank with external magnetic port.
+
+                    full features
+                    </p>
+                </div>
+                
+            </section>
             <p>Product Page</p>
             <Accordion items={
                 [
@@ -13,6 +46,7 @@ const Product = () => {
             } />
 
             <Carousel
+                sizeFull={true}
                 items={[
                   {
                     image:
@@ -32,8 +66,29 @@ const Product = () => {
             <style jsx>{`
                 // CSS-START
                 // insert here the CSS for this page
-                .container{
-                    padding: 5rem 7rem;
+                .container-fluid {
+                  width: 100%;
+                }
+                .flex {
+                    display: flex;
+                }
+                .flex-col {
+                    flex-direction: column;
+                }
+                .justify-content-center {
+                    justify-content: center
+                }
+                p {
+                    font-size: 16px;
+                }
+                .hero-section__description {
+                    font-family: 'Antic Didone';
+                    padding: 5rem 3rem;
+                    justify-content: start;
+                    align-items: center;
+                }
+                .flex-basis-50 {
+                    flex-basis: 50%;
                 }
                 // CSS-END
             `}</style>
