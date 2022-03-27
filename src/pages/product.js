30d891f8248152244d3c5f321f2ced31b71debaa
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2022-03-27 17:04:28 +0200

    Implemented Features section

--- src/pages/product.js
+++ src/pages/product.js
@@ -37,10 +37,73 @@ const Product = () => {
                 </div>
                 
             </section>
+
+            <section className="features">
+                <h2 className="text-center py-5">Full Features</h2>
+                <div className="features__icons flex justify-content-evenly">
+                    <div className="flex flex-col align-items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/253/253298.png" alt="Feature Icon" className="icon" />
+                        <p>icona feature</p>
+                    </div>
+                    <div className="flex flex-col align-items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/253/253298.png" alt="Feature Icon" className="icon" />
+                        <p>icona feature</p>
+                    </div>
+                    <div className="flex flex-col align-items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/253/253298.png" alt="Feature Icon" className="icon" />
+                        <p>icona feature</p>
+                    </div>
+                </div>
+                <div className="feature__card py-5 flex">
+                    <div className="feature__description flex-basis-50 py-2 px-5">
+                        <h3>Feature 1</h3>
+                        <div className="pl-3">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu aliquet tellus a sit nec nec scelerisque. Commodo, at feugiat ac facilisis quis mauris, nulla. Faucibus nibh commodo gravida vel enim. 
+                            </p>
+                            <Accordion 
+                                items={[
+                                    {
+                                        title: 'Materiale',
+                                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque magna in est consequat id nibh. Elit ornare quisque volutpat amet. Id phasellus at aenean egestas nulla.'
+                                    },
+                                    {
+                                        title: 'Dimensioni',
+                                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque magna in est consequat id nibh. Elit ornare quisque volutpat amet. Id phasellus at aenean egestas nulla.'
+                                    },
+                                    {
+                                        title: 'Lorem Ipsum',
+                                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque magna in est consequat id nibh. Elit ornare quisque volutpat amet. Id phasellus at aenean egestas nulla.'
+                                    }
+                                ]}
+                            />
+                        </div>
+                    </div>
+                    <div className="feature__image flex-basis-50">
+                        <img src="https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80" alt="" className="img-fluid" />
+                    </div>
+                    
+                </div>
+                <div className="feature__card py-5 flex">
+                    <div className="feature__description flex-basis-50 py-2 px-5">
+                        <h3>Feature 2</h3>
+                        <div className="pl-3">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu aliquet tellus a sit nec nec scelerisque. Commodo, at feugiat ac facilisis quis mauris, nulla. Faucibus nibh commodo gravida vel enim. 
+                            </p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu aliquet tellus a sit nec nec scelerisque. Commodo, at feugiat ac facilisis quis mauris, nulla. Faucibus nibh commodo gravida vel enim. 
+                            </p>
+                        </div>
+                    </div>
+                    <div className="feature__image flex-basis-50">
+                        <img src="https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80" alt="" className="img-fluid" />
+                    </div>
+                    
+                </div>
+            </section>
             
             <style jsx>{`
-                // CSS-START
-                // insert here the CSS for this page
                 .container-fluid {
                   width: 100%;
                 }
@@ -50,21 +113,82 @@ const Product = () => {
                 .flex-col {
                     flex-direction: column;
                 }
-                .justify-content-center {
-                    justify-content: center
-                }
-                p {
-                    font-size: 16px;
-                }
-                .hero-section__description {
-                    font-family: 'Antic Didone';
-                    padding: 5rem 3rem;
-                    align-items: start;
-                }
                 .flex-basis-50 {
                     flex-basis: 50%;
                 }
-                // CSS-END
+                .justify-content-center {
+                    justify-content: center;
+                }
+                .justify-content-evenly {
+                    justify-content: space-evenly;
+                }
+                .align-items-center {
+                    align-items: center;
+                }
+                .img-fluid {
+                    max-width: 100%;
+                }
+                h1, h2, h3, h4, h5, h6 {
+                    font-family: 'Antic Didone';
+                }
+                h1 {
+                    font-size: 70px;
+                }
+                h2 {
+                    font-size: 45px;
+                }
+                h3 {
+                    font-size: 30px;
+                }
+                p {
+                    font-size: 16px;
+                    font-family: 'Inter';
+                    font-weight: 300;
+                    line-height: 26px;
+                }
+                .py-2 {
+                    padding: 2rem 0;
+                }
+                .py-5 {
+                    padding: 5rem 0;
+                }
+                .px-5 {
+                    padding: 0 5rem;
+                }
+                .pl-3 {
+                    padding: 3rem;
+                }
+                .hero-section__description {
+                    padding: 5rem 3rem;
+                    align-items: start;
+                }
+                .feature__card:nth-child(2) {
+                    flex-direction: row-reverse;
+                }
+                .icon {
+                    width: 60px;
+                    height: 60px;
+                }
+                .text-center {
+                    text-align: center;
+                }
+                @media screen and (max-width: 992px) {
+                    .feature__description {
+                        padding: 0 2rem;
+                    }
+                }
+                @media screen and (max-width: 768px) {
+                  .hero-section {
+                    flex-direction: column;
+                  }
+                  .feature__card {
+                      flex-direction: column;
+                  }
+                  .features__icons {
+                      flex-direction: column;
+                  }
+                }
+                
             `}</style>
         </div>
     )
