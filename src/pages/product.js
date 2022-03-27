f7c6e67c9fe5ea941d1212b8978f57e7fc7e7c70
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2022-03-27 12:39:23 +0200

    Added p and link to hero section

--- src/pages/product.js
+++ src/pages/product.js
@@ -27,41 +27,16 @@ const Product = () => {
                 <div className="hero-section__description flex flex-col flex-basis-50">
                     <h1 className="">Clutch-e bag</h1>
                     <p>
-                    The first Smart Fashion Tech pochette in the world.
-                    The objective is that of bringing a new idea of handbag.
-
-                    Clutch-e Bag is equipped with fingerprint scanner for the opening, GPS tracking system, speaker to make it ring and find it easily everytime everywhere, removable power bank with external magnetic port.
-
-                    full features
+                      The first Smart Fashion Tech pochette in the world.
+                      The objective is that of bringing a new idea of handbag.
                     </p>
+                    <p>
+                      Clutch-e Bag is equipped with fingerprint scanner for the opening, GPS tracking system, speaker to make it ring and find it easily everytime everywhere, removable power bank with external magnetic port.
+                    </p>
+                    <a href="#">full features</a>
                 </div>
                 
             </section>
-            <p>Product Page</p>
-            <Accordion items={
-                [
-                    {title: "Titolo 1", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quo et! Obcaecati magnam excepturi consectetur iste. Doloremque consequatur beatae enim voluptates officia, cum exercitationem sapiente autem dignissimos iste voluptatum fuga."},
-                    {title: "Titolo 2", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quo et! Obcaecati magnam excepturi consectetur iste. Doloremque consequatur beatae enim voluptates officia, cum exercitationem sapiente autem dignissimos iste voluptatum fuga."},
-                ]
-            } />
-
-            <Carousel
-                sizeFull={true}
-                items={[
-                  {
-                    image:
-                      "https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-                  },
-                  {
-                    image:
-                      "https://images.unsplash.com/photo-1605733513597-a8f8341084e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1229&q=80"
-                  },
-                  {
-                    image:
-                      "https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-                  }
-                ]}
-              />
             
             <style jsx>{`
                 // CSS-START
@@ -84,8 +59,7 @@ const Product = () => {
                 .hero-section__description {
                     font-family: 'Antic Didone';
                     padding: 5rem 3rem;
-                    justify-content: start;
-                    align-items: center;
+                    align-items: start;
                 }
                 .flex-basis-50 {
                     flex-basis: 50%;
